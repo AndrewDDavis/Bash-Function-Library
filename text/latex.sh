c6b341d264e1fe1ec3391e7020cb972006fd0dc6
Andrew Davis <addavis@gmail.com>
2025-03-05 20:50:44 -0500

    (split)file reorg

--- text/latex.sh
+++
@@ -1,4 +0,0 @@
-# LaTeX -- MacTeX BasicTeX package preferred over fink's
-# - man path added via /etc/manpaths.d/TeX
-path_check_add '/usr/texbin'
-export TEXINPUTS=".//:"   # allow .sty files in subdirectories
