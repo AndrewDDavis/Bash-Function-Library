010fdfde70b1ca75d89d7a5889ecc618220c2524
Andrew Davis <addavis@gmail.com>
2025-03-05 21:09:10 -0500

    (split)split beet-rename and beet env into separate files

---
+++ media/beet-rename.sh
@@ -0,0 +1,21 @@
+beet-rename() {
+
+    : "Rename files on disk, and update the path in the beets library
+
+    Usage: beet-rename <oldpath> <newpath>
+
+    beet will ask for confirmation first.
+    "
+
+    [[ $# -lt 2  || $1 == @(-h|--help) ]] &&
+        { docsh -TD; return; }
+
+    local beet_cmd
+    beet_cmd=$( builtin type -P beet ) \
+        || return
+
+    /bin/mv -v "$1" "$2" \
+        || return
+
+    "$beet_cmd" modify -M "path:$1" "path=$2"
+}
