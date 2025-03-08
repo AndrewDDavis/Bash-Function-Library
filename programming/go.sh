89a462be0279c6159d098c779ff332e126ff9daf
Andrew Davis <addavis@gmail.com>
2025-03-08 00:31:14 -0500

    (split)mv to Env

--- programming/go.sh
+++
@@ -1,5 +0,0 @@
-# Google's Go language
-[[ -d $HOME/gopath ]] && {
-    export GOPATH=$HOME/gopath
-    path_check_add -f "$GOPATH" "$GOPATH/bin"
-}
--- programming/haskell.sh
+++
@@ -1,2 +0,0 @@
-# Haskell (for pandoc)
-path_check_add "$HOME/Library/Haskell/bin"
