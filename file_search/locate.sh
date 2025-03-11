9e1515e94e2761814f2839109726e13d0695e798
Andrew Davis <addavis@gmail.com>
2025-03-11 13:55:04 -0400

    (split)file reorg

--- file_search/locate.sh
+++
@@ -1,9 +0,0 @@
-# shellcheck shell=sh
-
-if [ "$( uname -s )" = Darwin ]
-then
-    # BSD locate on macOS
-    # - NB, updatedb on macOS is locate.updatedb
-    export LOCATE_CONFIG="$HOME/.config/locate/locate.rc"
-    export LOCATE_PATH="$HOME/.cache/locate/locate.database"
-fi
