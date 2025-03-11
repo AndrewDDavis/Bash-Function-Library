e206c2e3d104ce28e7cc4be5ae590bd4815fb84d
Andrew Davis <addavis@gmail.com>
2025-03-11 15:21:31 -0400

    (split)docs and style

--- file_search/lfcd.sh
+++ file_search/lfcd.sh
@@ -1,22 +1,19 @@
-# lf file manager
-
 lfcd () {
 
-    [[ $# -eq 0 || $1 == @(-h|--help) ]] && {
+    : "Change shell working dir using lf file manager
 
-	    docsh -TD "Change shell working dir using lf
+        You may also like to assign a key (e.g. Ctrl-O) to this command, e.g.:
 
-        You may also like to assign a key (e.g. Ctrl-O) to this command:
+          bind '\"\\C-o\":\"lfcd\\C-m\"'  # bash
+          bindkey -s '^o' 'lfcd\\n'  # zsh
+    "
 
-          bind '"\C-o":"lfcd\C-m"'  # bash
-          bindkey -s '^o' 'lfcd\n'  # zsh
-	    "
-	    return 0
-    }
+    [[ $# -eq 0  || $1 == @(-h|--help) ]] &&
+        { docsh -TD; return; }
 
     local _lfwd
-    # `command` is needed in case `lfcd` is aliased to `lf`
 
+    # - use 'command' in case `lfcd` is aliased to `lf`
     if _lfwd=$( command lf -print-last-dir "$@" )
     then
         cd "$_lfwd"
