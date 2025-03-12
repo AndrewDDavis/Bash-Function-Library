c7e279b7cbd06cf279340738bdf47d1a43eb109e
Andrew Davis <addavis@gmail.com>
2025-03-11 23:05:45 -0400

    (split)syle

--- research/d2n.sh
+++ research/d2n.sh
@@ -1,18 +1,21 @@
-# DCM2NII (dcm2niix, Rorden's tool)
 d2n() {
 
-    : "dcm2niix wrapper
+    : "Wrapper function for Rorden's dcm2niix tool
 
-    Calls dcm2niix with my usual settings on dicoms found in the passed dir
+        Calls dcm2niix with my usual settings on dicoms found in the passed dir
 
-      - Recursively checks 5 levels of depth for dicoms by default
-      - If no dir supplied, uses the current directory
-      - NIfTI files are output in the current working directory
-      - Additional options may be supplied, as long as the search dir is the last argument
+          - Recursively checks 5 levels of depth for dicoms by default
+          - If no dir supplied, uses the current directory
+          - NIfTI files are output in the current working directory
+          - Additional options may be supplied, as long as the search dir is the last argument
     "
 
     [[ -n ${1-} ]] ||
         set -- '.'
 
-    dcm2niix -o '.' -z 'y' -b 'y' -f '%d' "$@"
+    local d2n_cmd
+    d2n_cmd=$( builtin type -P dcm2niix ) \
+        || return 5
+
+    "$d2n_cmd" -o '.' -z 'y' -b 'y' -f '%d' "$@"
 }
