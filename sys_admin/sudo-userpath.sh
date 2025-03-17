68263b322e6d31afe05073aad1c9484d8c9e4caf
Andrew Davis <addavis@gmail.com>
2025-03-17 14:45:21 -0400

    (split)file reorg

---
+++ sys_admin/sudo-userpath.sh
@@ -0,0 +1,15 @@
+sudo-userpath() {
+
+    : "Run sudo command, keeping the user's PATH
+
+        Usage: sudop <command>
+
+        All arguments are passed through to sudo, after setting PATH to the user's
+        PATH variable.
+    "
+
+    [[ $# -eq 0  || $1 == @(-h|--help) ]] &&
+        { docsh -TD; return; }
+
+    sudo PATH="$PATH" "$@"
+}
---
+++ sys_admin/sudoa.sh
@@ -0,0 +1,2 @@
+# allow alias expansion in sudo command
+alias sudoa="sudo "
