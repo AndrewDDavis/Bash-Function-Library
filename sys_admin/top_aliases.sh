caf936209a6038771ba16d853ece1f20b235b5b3
Andrew Davis <addavis@gmail.com>
2025-03-17 17:57:03 -0400

    (split)file reorg

---
+++ sys_admin/top_aliases.sh
@@ -0,0 +1,23 @@
+if [[ $( command top --version 2>/dev/null ) == *procps-ng* ]]
+then
+    # Linux top (from procps package)
+
+    top-cpu() {
+
+        : "Order display by CPU usage
+
+            Calls top using -o, which orders the display by key (default pid).
+        "
+
+        top -ocpu "$@"
+    }
+
+    top-sb() {
+
+        : "Prevent top from clobbering the scrollback buffer"
+
+        tput smcup
+        top "$@"
+        tput rmcup
+    }
+fi
