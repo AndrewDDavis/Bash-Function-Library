fd99f257393c6e347299c9ad7ef47eabb50f3616
Andrew Davis <addavis@gmail.com>
2025-03-17 14:24:06 -0400

    (split)style

--- sys_admin/fink.sh
+++
@@ -1,6 +0,0 @@
-# Fink setup
-[[ -r /sw/bin/init.sh ]] && source /sw/bin/init.sh
-# should let MacTeX's ghostscript in /usr/local/bin rule?
-
-# finkudprep notification
-#[[ -r /etc/finkudprep-motd ]] && cat /etc/finkudprep-motd
--- sys_admin/nix.sh
+++ sys_admin/nix.sh
@@ -1,4 +1,3 @@
-# Nix
-
-# run with increased verbosity by default
-[[ -n $( command -v nix ) ]] && alias nix="nix -v"
+# Run Nix with increased verbosity by default
+[[ -n $( command -v nix ) ]] &&
+    alias nix="nix -v"
