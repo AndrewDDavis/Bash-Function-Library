714ef6bec216a882bd9b082de6b21e4bed4a44ea
Andrew Davis <addavis@gmail.com>
2025-03-26 21:26:17 -0400

    rename alias

--- lib/file_props/eza.sh
+++ lib/file_props/eza.sh
@@ -1,6 +1,5 @@
 # Eza, tries to replace interactive ls + tree
-alias eza-tree="eza -Tlo --no-permissions"
+alias treeza="eza -Tlo --no-permissions"
 
 # maybe this will help me remember the name
 alias ls-eza="eza"
-alias tree-eza="eza-tree"
