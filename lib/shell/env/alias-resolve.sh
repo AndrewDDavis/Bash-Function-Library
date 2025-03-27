5a466cb2074ea027f655bdefd2665184715d4efa
Andrew Davis <addavis@gmail.com>
2025-03-26 21:27:11 -0400

    give it a more shell-friendly name; add option to print result

--- lib/shell/env/alias-resolve.sh
+++ lib/shell/env/alias-resolve.sh
@@ -1,63 +1,91 @@
-alias_rslv() {
+# deps
+import_func array_strrepl str_to_words \
+    || return 63
 
-    # function docs
-    [[ $# == 0  ||  $1 == -h ]] && {
+alias-resolve() {
 
-        : "resolve alias(es) recursively
+    : "Expand an alias to a command line, using recursion if necessary
 
-        Usage: alias_rslv [-e] <command> <array-name>
+    Usage: alias-resolve [options] <name> [array-name]
 
-        Checks whether an alias is defined for 'command' in the current execution
-        environment.
-        - If not, 'array-name' will contain only 1 element, the command itself.
-        - If so, 'array-name' will contain the command alias split into words, including
-          any arguments and variable definitions in the alias. Furthermore, if the
-          resulting command is an alias, it is resolved recursively until no new command
-          results from the process.
+    This function checks whether 'name' is defined as an alias in the current
+    environment. The result is written to 'array-name' if supplied, or to an array
+    called CMD_ALIAS.
 
-        This function allows the use of command aliases in shell functions. Aliases are
-        expanded in functions if they are defined when the function definition is read,
-        and the shell is interactive, or the 'expand_aliases' option is set at that
-        time. This is not usually the case when the shell is executing rc files and
-        reading function definitions at startup.
+    If no alias was found, the resulting array will contain only 1 element, the
+    command itself. Otherwise, the array will contain the command alias split into
+    words, including any arguments and variable definitions from the alias. If the
+    alias initially resolves to another alias, the procedure repeats recursively
+    until the resulting command is not an alias.
 
-        Care is needed to handle aliases that include variable assignments before the
-        command, e.g. alias ls='LC_COLLATE=en_CA.utf8 ls --color=auto'. When this
-        captured in a variable, even when split into an array, the shell won't run it as
-        usual, since A=B is interpreted as a command. In this case, 'env' can be added
-        to the start of the command array, e.g.:
+    This function facilitates the use of command aliases in shell functions. In the
+    Bash shell, aliases may be expanded in functions if they were defined when the
+    function definition was read, and either the shell was interactive or the
+    'expand_aliases' option was set. In a typical setup where the function
+    definitions are read during the shell initialization process, the aliases will
+    not be expanded.
 
-          [[ \${cmd_words[0]} == *=* ]] &&
-              cmd_words=( env \"\${cmd_words[@]}\" )
+    Care is needed to handle aliases that include variable assignments before the
+    command, e.g. alias ls='LC_COLLATE=en_CA.utf8 ls --color=auto'. After such a
+    command is captured in an array variable, the shell would normally fail to run
+    it, since it would try to interpret the variable assignment as a command. In
+    such cases, alias-resolve adds 'env' to the start of the command array so the
+    command will run as expected, unless the -e option is used.
 
-        The '-e' option to this function adds 'env' as the first element of the array,
-        if the first element would otherwise be a variable assignment.
+    Options
 
-        Examples
+      -e
+      : Prevent adding 'env' as the first array element, even if the array would
+        otherwise start with a variable assignment.
 
-          alias_rslv ll ls_cmd
-          # ls_cmd may be: ( LC_COLLATE=C.utf8 ls --color=auto -lh )
-        "
-        docsh -TD
-        return
-    }
+      -p
+      : Print the resulting expansion to STDOUT after resolving the alias.
 
-    # env option
-    local _e=''
-    [[ $1 == -e ]] && { _e=1; shift; }
+    Examples
+
+      alias-resolve ll ls_cmd
+      # ls_cmd may be: ( LC_COLLATE=C.utf8 ls --color=auto -lh )
+    "
+
+    # defaults and options
+    local _env=1 _pr
+
+    local flag OPTARG OPTIND=1
+    while getopts ':ehp' flag
+    do
+        case $flag in
+            ( e ) _env='' ;;
+            ( h ) docsh -TD; return ;;
+            ( p ) _pr=1 ;;
+            ( \? ) err_msg 2 "unknown option: '-$OPTARG'"; return ;;
+            ( : )  err_msg 2 "missing argument for -$OPTARG"; return ;;
+        esac
+    done
+    shift $(( OPTIND-1 ))
+
+    # positional args
+    (( $# < 3 )) ||
+        { err_msg 5 "too many arguments: '$*'"; return; }
 
     # posn args
-    [[ $# -eq 2 ]] || return 2
+    local name=${1:?"name required"}
+    shift
 
-    local cmd=$1
-    local -n al_words=$2
-    shift 2
+    local -n al_words
+    if (( $# > 0 ))
+    then
+        al_words=$1
+        shift
+    else
+        al_words=CMD_ALIAS
+    fi
 
     # return false for no current alias definition
-    builtin alias "$cmd" &>/dev/null \
+    builtin alias "$name" &>/dev/null \
         || return 1
 
     # Recursively resolve the alias, up to 100 iterations
+    local cmd=$name
     al_words=( "$cmd" )
     local i=0 new_words=() new_cmd
 
@@ -66,25 +94,35 @@ alias_rslv() {
         # fetch defined alias and split into words, respecting quoting
         str_to_words -q new_words "${BASH_ALIASES[$cmd]}"
 
-        # replace cmd in the al_words array with its alias words (e.g. ll -> ls -l)
+        # replace cmd in the al_words array with its alias words
+        # - e.g. ll -> ls -l
         array_strrepl al_words "$cmd" "${new_words[@]}"
 
         # identify the new command word, after any env var assignments
         new_cmd=$( array_match -p new_words '^[^=]+$' )
 
         # detect recursive alias
-        [[ $new_cmd == "$cmd" ]] && break
+        [[ $new_cmd == "$cmd" ]] &&
+            break
+
         cmd=$new_cmd
 
         # limit runaway loop
-        (( i++ ))
-        [[ $i -gt 99 ]] &&
-            { err_msg 99 "too many iterations"; return; }
+        (( ++i ))
+        (( i > 99 )) && {
+            err_msg 99 "too many iterations"
+            return
+        }
     done
 
-    # add env?
-    if [[ -n $_e  &&  ${al_words[0]} == *=* ]]
+    if [[ -n $_env  && ${al_words[0]} == *=* ]]
     then
+        # add env cmd
         al_words=( env "${al_words[@]}" )
+    fi
+
+    if [[ -v _pr ]]
+    then
+        printf '%s: %s\n' "$name" "${al_words[*]}"
     fi
 }
