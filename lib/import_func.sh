fdb06abef14274716e1399587f75ff0650287245
Andrew Davis <addavis@gmail.com>
2025-03-28 15:19:46 -0400

    docs; simplify code; limit search to .sh and .bash files; e.g. in docs to import all env files

--- lib/import_func.sh
+++ lib/import_func.sh
@@ -11,12 +11,14 @@ import_func() {
             import_func -a [file name] ...
 
         In its default mode of operation (without -a), this function searches a
-        directory tree for function definitions matching the requested name, and imports
-        the function into the current shell by sourcing its file.
+        directory tree for function definitions matching the specified name(s). The
+        functions are then imported into the current shell session by applying the
+        source built-in command to the relevant files.
 
-        By default, the search is performed in ~/.bash_lib/, but the library path
-        may be overridden by setting the BASH_FUNCLIB variable. Symlinks within the
-        library are dereferenced and followed.
+        By default, the ~/.bash_lib/ directory is searched, but the library path may
+        be overridden by setting the BASH_FUNCLIB variable. Symlinks within the library
+        are dereferenced and followed. Source files are expected to have a .sh or .bash
+        file extension.
 
         Options
 
@@ -32,9 +34,10 @@ import_func() {
             function with the same name is already defined. This option forces such
             functions to be reimported by sourcing the relevant file.
 
-        Example: import dependencies in a script
+        Example 1
 
-          # import dependencies
+          # import dependencies in a script
+
           [[ \$( builtin type -t import_func ) == function ]] || {
               source ~/.bash_lib/import_func.sh \\
                   || return 63
@@ -43,10 +46,12 @@ import_func() {
           import_func docsh err_msg \\
               || return 62
 
+        Example 2
 
-          # NB, in the script files that define functions in the Bash_Function_Library,
-          # access to import_func will be assumed, and only the import_func line above
-          # will be used.
+          # import all source files within a directory tree, e.g. for environment setup
+          # in .bashrc
+
+          BASH_FUNCLIB=~/.bashrc.d import_func -a
         "
         docsh -TD
         return
@@ -54,7 +59,7 @@ import_func() {
 
     # clean-up routine
     trap '
-        unset -f _fn_list
+        unset -f _xfn
         trap - return
     ' RETURN
 
@@ -73,74 +78,72 @@ import_func() {
     shift $(( OPTIND-1 ))
 
     # check for expected library path
-    local libdir
+    local libdir="$HOME"/.bash_lib
 
-    if [[ -v BASH_FUNCLIB ]]
-    then
+    [[ -n ${BASH_FUNCLIB-} ]] &&
         libdir=$BASH_FUNCLIB
-    else
-        libdir=~/.bash_lib
-    fi
 
     [[ -d $libdir ]] ||
         { err_msg 9 "libdir not found: '$libdir'"; return; }
 
 
+    _xfn() {
+
+        # add find args to exclude filename, possibly adding suffixes
+        if [[ $1 == */ ]]
+        then
+            # directory
+            find_cmd+=( -name "${1%/}" )
+
+        elif [[ $1 == *@(.sh|.bash) ]]
+        then
+            # already has extension
+            find_cmd+=( -name "$1" )
+
+        else
+            find_cmd+=( -name "${1}.sh" -o -name "${1}.bash" )
+        fi
+    }
+
+
     if [[ -v _all ]]
     then
         # import all files except those specified
-        local fn fnxs=() find_cmd find_cmdline
 
-        find_cmd=$( builtin type -P find ) \
-            || return
+        # - build a find command line to match filenames in the lib dir
+        local find_cmd fn
 
-        find_cmdline=( "$find_cmd" -L "$libdir" )
+        find_cmd=( "$( builtin type -P find )" -L "$libdir" ) \
+            || return 9
 
         if [[ $# -gt 0 ]]
         then
-            # build file exclusion list using the construct:
-            # find ... \( -name .fdignore -o -name 'a file' \) -prune -o ... -print0
-            find_cmdline+=( '(' )
+            # - build file exclusion list using the construct:
+            #   find ... \( -name .fdignore -o -name 'a file' \) -prune -o ... -print0
+            find_cmd+=( '(' )
 
-            _fn_list() {
-
-                # add 1 or 2 filenames depending on filename suffix
-
-                if [[ $1 == */ ]]
-                then
-                    fnxs=( -name "${1%/}" )
-
-                elif [[ $1 == *@(.sh|.bash) ]]
-                then
-                    fnxs=( -name "$1" )
-
-                else
-                    fnxs=( -name "${1}.sh" -o -name "${1}.bash" )
-                fi
-            }
-
-            _fn_list "$1"
-            find_cmdline+=( "${fnxs[@]}" )
+            _xfn "$1"
 
             for fn in "${@:2}"
             do
-                _fn_list "$fn"
-                find_cmdline+=( -o "${fnxs[@]}" )
+                find_cmd+=( -o )
+                _xfn "$fn"
             done
 
-            find_cmdline+=( ')' -prune -o )
+            find_cmd+=( ')' -prune -o )
         fi
 
         # - NB, type f matches symlinked files, since we're using -L
-        find_cmdline+=( -type f \( -name '*.sh' -o -name '*.bash' \) -print0 )
+        find_cmd+=( -type f \( -name '*.sh' -o -name '*.bash' \) -print0 )
 
-        # run find and import the selected files
+
+        # Run find and import the selected files
         while IFS='' read -rd '' fn <&3
         do
             # shellcheck source=/dev/null
             source "$fn"
 
-        done 3< <( "${find_cmdline[@]}" )
+        done 3< <( "${find_cmd[@]}" )
 
         # NB, an alternative, with globstar set and filenames without newlines, would be
         # to use e.g.:
@@ -152,26 +155,30 @@ import_func() {
         [[ $# -gt 0 ]] ||
             return 99
 
-        local fn src_fns grep_cmd grep_ptn grep_cmdline
+        # grep path and opts (recursive ERE, follow symlinks, limit to text-format files)
+        local grep_cmd grep_ptn
 
-        grep_cmd=$( builtin type -P grep ) \
-            || return
+        grep_cmd=( "$( builtin type -P grep )" -EIRl ) \
+            || return 9
 
-        for fn in "$@"
+        # limit to .sh and .bash filenames
+        grep_cmd+=( --include='*.sh' --include='*.bash' )
+
+        local func_nm src_fns
+
+        for func_nm in "$@"
         do
-            if [[ $( builtin type -at "$fn" ) == *function* \
-                    && ! -v _force ]]
+            if  [[ $( builtin type -at "$func_nm" ) == *function*
+                && ! -v _force ]]
             then
                 # skip existing function
                 continue
             fi
 
-            # search for the source file with grep
-            grep_ptn="^(${fn}[[:blank:]]*\(\)|function[[:blank:]]+${fn})"
+            # match pattern for function definition in source file
+            grep_ptn="^(${func_nm}[[:blank:]]*\(\)|function[[:blank:]]+${func_nm})"
 
-            grep_cmdline=( "$grep_cmd" -EIRl -e "$grep_ptn" "$libdir" )
-
-            mapfile -t src_fns < <( "${grep_cmdline[@]}" )
+            mapfile -t src_fns < <( "${grep_cmd[@]}" -e "$grep_ptn" "$libdir" )
 
             if [[ ${#src_fns[@]} -eq 1 ]]
             then
@@ -180,13 +187,13 @@ import_func() {
 
             elif [[ ${#src_fns[@]} -eq 0 ]]
             then
-                err_msg 1 "no source found for '$fn'"
+                err_msg 1 "no source found for '$func_nm'"
                 return
 
             else
                 err_msg 1 \
-                    "multiple source files found for '$fn'" \
-                    "command line was '${grep_cmdline[*]}'"
+                    "multiple source files found for '$func_nm'" \
+                    "command line was '${grep_cmd[*]} -e $grep_ptn $libdir'"
                 return
             fi
         done
