7b546dd00fd709e1688f205dfba66b076ca8e54c
Andrew Davis <addavis@gmail.com>
2025-04-01 22:45:32 -0400

    init

---
+++ lib/text/wrap-txt.sh
@@ -0,0 +1,37 @@
+wrap-txt() {
+
+    : "Wrap text
+
+        Usage: wrap-txt [fmt-options] [file]
+
+        Calls 'fmt -u' to reformat the text so that paragraphs wrap near the character
+        limit (default 88). When no file is specified, or when file is -, standard
+        input is used.
+
+        Notable fmt options:
+
+          -c
+          : preserve indentation of first two lines
+
+          -p STRING
+          : only reformat lines beginning with STRING, and retain the prefix
+
+          -s
+          : only split long lines, do not refill
+
+          -u
+          : uniform spacing, one space between words, two after sentences
+
+          -w N
+          : maximum line width
+    "
+
+	[[ ${1-} == @(-h|--help) ]] &&
+    	{ docsh -TD; return; }
+
+    local fmt_cmd
+    fmt_cmd=( "$( builtin type -P fmt )" -u -w88 ) \
+        || return 9
+
+    "${fmt_cmd[@]}" "$@"
+}
