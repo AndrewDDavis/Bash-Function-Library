80ebaf25f580f3430ae7e6109d732650ffa54b9f
Andrew Davis <addavis@gmail.com>
2025-04-07 22:44:58 -0400

    code cleanup

--- lib/shell/arrays/array_max.sh
+++ lib/shell/arrays/array_max.sh
@@ -14,36 +14,47 @@ array_max() {
     }
 
     # nameref to array
-    local -n __earr__=$1 || return
-    shift 1
+    local -n __earr__=$1 \
+        || return
+    shift
 
-    [[ $# -eq 0 ]] || return 2
+    [[ $# -eq 0 ]] ||
+        return 2
+
 
     # ensure we got a non-empty array
-    [[ -v __earr__[@]  &&  ${__earr__@a} == *[aA]* ]] ||
-        { err_msg 3 "need a non-empty array, got '${!__earr__}: $( declare -p ${!__earr__} )'"; return; }
+    # [[ -v __earr__[*]  && ${#__earr__[*]} -gt 0 ]] ||
+    [[ -v __earr__[*]  && ${__earr__@a} == *[aA]* ]] ||
+        { err_msg 3 "need a non-empty array, got '${!__earr__}'"; return; }
 
-    trap 'unset -f _chk' RETURN
+    trap 'unset -f _elem_isint' RETURN
 
-    _chk() {
+    _elem_isint() {
 
-        is_int ${__earr__[$1]} ||
+        is_int "${__earr__[$1]}" ||
             { err_msg 4 "${!__earr__}[$1] not an int: '${__earr__[$1]}'"; return; }
     }
 
-    # loop over the array to find the max
-    local i idcs _max
 
-    idcs=( ${!__earr__[@]} )
+    # loop over array to find the max
+    local i _max idcs
+
+    # indices of referenced array
+    idcs=( "${!__earr__[@]}" )
 
     i=${idcs[0]}
-    _chk $i || return
+    _elem_isint "$i" \
+        || return
+
     _max=${__earr__[$i]}
 
-    for i in ${idcs[@]:1}
+    for i in "${idcs[@]:1}"
     do
-        _chk $i || return
-        [[ ${__earr__[$i]} -le $_max ]] || _max=${__earr__[$i]}
+        _elem_isint "$i" \
+            || return
+
+        (( ${__earr__[$i]} <= _max )) \
+            || _max=${__earr__[$i]}
     done
 
     printf '%s\n' "$_max"
