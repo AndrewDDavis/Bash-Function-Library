15017cd827217510a3bcde3d187a3bb31e2a0021
Andrew Davis <addavis@gmail.com>
2025-04-10 12:29:26 -0400

    turn on verbosity without the subshell, just manage set -x output

--- lib/shell/scripting/run_vrb.sh
+++ lib/shell/scripting/run_vrb.sh
@@ -1,12 +1,18 @@
+# deps
+import_func is_int \
+    || return
+
 run_vrb() {
 
     [[ $# -eq 0  ||  $1 == @(-h|--help) ]] && {
 
         : "Print command line to STDERR as it is being run
 
-        Usage
+        Usage: run_vrb [options] [--] <command-line ...>
 
-          run_vrb [options] [--] <command-line ...>
+        This function runs the command line with the xtrace (set -x) option enabled.
+        This causes the command line to be printed on STDERR as it is executed,
+        prepended by the \$PS4 string (currently '$( sed -E 's/\\(\[|\])//g' <<< $PS4 )').
 
         Options
 
@@ -19,13 +25,14 @@ run_vrb() {
             modify the command line to be explicit about what is run (i.e. either
             'builtin command' or '/path/to/command').
 
-        This function starts a subshell and issues \`set -x\`, which causes the command
-        line to be printed on STDERR, prepended by \$PS4 (currently '$( sed -E 's/\\(\[|\])//g' <<< $PS4 )').
-
         If environment variable settings in the form of A=B are used at the start of the
-        command line, the \`env\` command is added to ensure the command runs. Note that
-        \`env\` is incompatible with running shell functions or builtins, so a command line
-        like \`'A=B run_vrb command args'\` is recommended.
+        command line, the env command is prepended to ensure the command runs. Note that
+        env is incompatible with running shell functions or builtins, so a command line
+        like 'A=B run_vrb command args' is recommended.
+
+        The return status code is usually the return status of the command line, but may
+        be 2 if there was an option problem, or 124-6 if there was a problem determining
+        the command name or type.
 
         Examples
 
@@ -34,101 +41,105 @@ run_vrb() {
           _v=1
           [[ \$foo == bar ]] && (( _v++ ))
           run_vrb -\$_v borg-go create
-
-        Return status
-
-        Usually the return status of the command, otherwise 2 if there is an option
-        problem, or 124-6 if there is a problem determining the command name or type.
         "
         docsh -TD
         return
     }
 
-    # Options
-    # - NB, getopts respects '--' and advances OPTIND
-    local _v=2 _P
+    ## Defaults and options
+    # - NB, getopts breaks the loop on '--' and advances OPTIND
+    local _v=2 setx _P
 
     local flag OPTARG OPTIND=1
     while getopts ':v:P' flag
     do
         case $flag in
-        ( v ) _v=$OPTARG ;;
+        ( v )
+            is_int "$OPTARG" \
+                || return
+            _v=$OPTARG
+        ;;
         ( P ) _P=1 ;;
         ( \? ) err_msg 2 "unknown option: '$OPTARG'"; return ;;
         ( : ) err_msg 2 "missing argument for '$OPTARG'"; return ;;
         esac
     done
-    shift $(( OPTIND - 1 ))
+    shift $(( OPTIND-1 ))
 
 
-    ## split the command line into A=B env var parts, the command name, and args
-    # - NB, I used to use array_match() in here, but that set up a function loop
-    local i cmd _env evars=() args=()
+    # check for set -x in the current shell
+    (( _v > 1 )) && [[ $- != *x* ]] \
+        && setx=1
+
+
+    ## Further arguments form the command line to be executed
+    # - split it into A=B args, the command name, and command args
+    # - NB, I used to use array_match() in here, but that caused a function loop (!)
+    local i _env eargs=() \
+        cmd=() args=()
 
     for (( i=1; i<=$#; i++ ))
     do
-        if [[ $i == 1  &&  ${!i} == @(env|*/env) ]]
+        if [[ $i -eq 1  &&  ${!i} == @(env|*/env) ]]
         then
+            # env command
             _env=1
-            evars+=( "${!i}" )
+            eargs+=( "${!i}" )
 
         elif [[ ${!i} == *=* ]]
         then
-            evars+=( "${!i}" )
+            # env-var setting
+            eargs+=( "${!i}" )
 
         else
-            cmd=${!i}
+            # command, followed by args
+            cmd[0]=${!i}
+            (( i++ ))
+            args+=( "${@:i}" )
+            shift $#
             break
         fi
     done
 
-    [[ -v cmd ]] || return 124
-
-    # command arguments
-    local j=$(( i + 1 ))
-    args+=( "${@:j}" )
-
-    shift $#
+    [[ -v cmd[*] ]] \
+        || return 124
 
 
-    ## if A=B env var settings are present, the env command is needed
-    [[ ${#evars[@]} -gt 0  &&  -z ${_env-} ]] &&
-        evars=( "$( builtin type -P env )" "${evars[@]}" )
+    # if A=B env var settings are present, the env command is needed
+    [[ ${#eargs[*]} -gt 0  && ! -v _env ]] \
+        && eargs=( "$( builtin type -P env )" "${eargs[@]}" )
 
 
-    [[ -n ${_P-} ]] && {
-
+    if [[ -v _P ]]
+    then
         # resolve command to builtin or file
-        # - type -t returns alias, keyword, function, builtin, file or ''
-        local t vct types=()
-
-        types=( $( builtin type -at "$cmd" ) ) \
+        # - NB, 'type -at' returns multi-line string containing alias, keyword,
+        #   function, builtin, file or ''
+        local types
+        types=$( builtin type -at "${cmd[0]}" ) \
             || return 125
 
-        for t in "${types[@]}"
-        do
-            [[ $t == @(alias|keyword|function) ]] && continue
-            vct=$t
-            break
-        done
-
         # adjust command argument
-        if [[ $vct == builtin ]]
+        if [[ $types == *builtin* ]]
         then
-            cmd=( 'builtin' "$cmd" )
+            cmd=( builtin "${cmd[0]}" )
 
-        elif [[ $vct == file ]]
+        elif [[ $types == *file* ]]
         then
-            # type -P gives path to executable
-            cmd=$( builtin type -P "$cmd" )
+            cmd[0]=$( builtin type -P "${cmd[0]}" )
 
         else
             return 126
         fi
-    }
+    fi
 
-    (
-        [[ $_v -gt 1 ]] && set -x
-        "${evars[@]}" "${cmd[@]}" "${args[@]}"
-    )
+
+    # run command line
+    [[ -v setx ]] \
+        && set -x
+
+    "${eargs[@]}" "${cmd[@]}" "${args[@]}"
+
+    { [[ -v setx ]] \
+        && set +x; } 2>/dev/null
 }
