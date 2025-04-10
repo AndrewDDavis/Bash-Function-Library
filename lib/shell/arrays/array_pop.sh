1fdce0d7f94c14100110cc092d6309c4b2d8b0c8
Andrew Davis <addavis@gmail.com>
2025-04-10 14:37:59 -0400

    try to avoid calling sort if possible

--- lib/shell/arrays/array_pop.sh
+++ lib/shell/arrays/array_pop.sh
@@ -1,18 +1,23 @@
+# deps
+import_func array_sort argmax \
+    || return
+
 array_pop() {
 
-    : "Remove element(s) from an array and decrement the index of later values
+    : "Remove array element(s) and decrement later indices
 
-    Usage: array_pop <array-name> <index> [index ...]
+    Usage: array_pop <array-name> [index ...]
 
-    If the indexed element does not exist, the array is left untouched.
+    If no index is provided, 0 is used. If an indicated array element does not exist,
+    a warning is printed. Providing pre-sorted indices speeds up execution.
 
-    To remove array elements without worrying about the index, or for associative
-    arrays, just use \`unset\`:
+    To remove array elements without modifying the index, or for associative arrays,
+    use the unset built-in:
 
       unset iarr[1] iarr[3]
       unset aarr[foo]
 
-    To remove gaps from the index, use array_reindex().
+    To remove all gaps from an array's index, use array_reindex.
 
     Examples
 
@@ -25,7 +30,7 @@ array_pop() {
       # declare -a abcs=([0]=\"b\" [1]=\"d\" [2]=\"e\" [3]=\"f\")
     "
 
-    [[ $# -lt 2  || $1 == @(-h|--help) ]] &&
+    [[ $# -eq 0  || $1 == @(-h|--help) ]] &&
         { docsh -TD; return; }
 
     # nameref to array
@@ -34,39 +39,97 @@ array_pop() {
     shift
 
     # check valid array
-    [[ -v __iarr__[@]  &&  ${__iarr__@a} == *a* ]] ||
+    [[ -v __iarr__[*]  && ${__iarr__@a} == *a* ]] ||
         { err_msg 3 "not an indexed array: '${!__iarr__}'"; return; }
 
+    # last array index
+    local m
+    m=$( argmax "${!__iarr__[@]}" )
 
-    # sort indices as decreasing, for efficiency
-    local k idcs i j
+    # default index to remove is 0
+    (( $# == 0 )) \
+        && set -- 0
 
-    idcs=( $( sort -gr < <( printf '%s\n' "$@" ) ) )
+    # check args are valid indices
+    local i idcs=()
+    for (( i=1; i<=$#; i++ ))
+    do
+        is_int ${!i} ||
+            { err_msg 4 "not a valid index: '${!i}'"; return; }
+
+        if [[ -v __iarr__[${!i}] ]]
+        then
+            idcs+=( "${!i}" )
+        else
+            err_msg w "${!__iarr__} has no element at ${!i}"
+        fi
+    done
     shift $#
 
-    for k in "${idcs[@]}"
-    do
-        is_int $k ||
-            { err_msg 4 "not an integer index: '$k'"; return; }
+    local n=${#idcs[*]}
 
-        [[ -v __iarr__[$k] ]] ||
-            continue
+    if (( n == 0 ))
+    then
+        return 0
 
-        unset __iarr__[$k]
+    elif (( n == 1 ))
+    then
+        # Unset idx and decrement later values
+        local j=${idcs[0]}
+        unset '__iarr__[j]'
 
-        # reindex later values
-        # array_reindex -c$k "${!__iarr__}"
-
-        # actually, retain any existing gaps
-        for i in "${!__iarr__[@]}"
+        for (( i=j+1; i<=m; i++ ))
         do
-            # ignore earlier values
-            (( i < k )) && continue
-
-            # later indices must be decremented by 1
-            j=$(( i - 1 ))
-            __iarr__[$j]=${__iarr__[$i]}
-            unset __iarr__[$i]
+            if [[ -v __iarr__[i] ]]
+            then
+                __iarr__[i-1]=${__iarr__[i]}
+                unset '__iarr__[i]'
+            fi
         done
-    done
+    else
+        # Sort the indices, so we can step through the array only once
+        # - NB, for small n, calling sort takes more than half the execution time!
+        #   With n=1, skipping the sort takes the time from ~15 ms to ~7ms.
+        # - Providing sorted indices speeds up the execution back to ~ 7 ms.
+        # So it's worth check whether the indices are already sorted.
+        local srtd=1 j=${idcs[0]}
+        for i in "${idcs[@]:1}"
+        do
+            (( i > j )) \
+                || { srtd=0; break; }
+            j=$i
+        done
+
+        (( srtd == 1 )) \
+            || array_sort idcs -n
+
+        # Unset idcs and decrement later values as we go
+        # - k tracks index of the idcs array, and the number of elements that
+        #   have been removed so far
+        # - j tracks the index of the next element to be removed
+        local j=${idcs[0]} k=1
+        unset '__iarr__[j]'
+        j=${idcs[1]}
+
+        for (( i=(idcs[0]+1); i<=m; i++ ))
+        do
+            if (( i == j ))
+            then
+                unset '__iarr__[i]'
+                (( ++k ))
+                if [[ -v idcs[k] ]]
+                then
+                    j=${idcs[k]}
+                else
+                    j=0
+                fi
+
+            elif [[ -v __iarr__[i] ]]
+            then
+                # decrement index of later elements
+                __iarr__[i-k]=${__iarr__[i]}
+                unset '__iarr__[i]'
+            fi
+        done
+    fi
 }
