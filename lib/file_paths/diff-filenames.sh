eb67abdffcce5fe355d0de0378c20ae096205d36
Andrew Davis <addavis@gmail.com>
2025-04-23 23:40:04 -0400

    bugfix: handle repeated file basenames

--- lib/file_paths/diff-filenames.sh
+++ lib/file_paths/diff-filenames.sh
@@ -2,16 +2,19 @@ diff-filenames() {
 
     : "Compare filenames among directories
 
-    Usage: diff-filenames [-s] <path1> <path2> [...] [search-terms]
+    Usage: diff-filenames [-s] <path1> <path2> [search-options]
 
     The \`find\` command is used to compare filenames from the specified directory
-    paths, and only unique filenames are printed. All search terms are passed to
+    paths, and only unique filenames are printed. All search options are passed to
     \`find\`.
 
     If the '-s' option is used, this function will explicitly report when all
     filenames are identical.
 
-    To compare the contents of files, use \`rsync\` or \`diff\`.
+    This may also be accomplished with 'diff -qr <path1> <path2>', which reports \"Only
+    in ...\" lines. However, that command is slower since it also compares the contents
+    of files with the same name. The \`rsync\` command may also be used to compare files
+    based on their content or metadata.
 
     Examples
 
@@ -23,33 +26,49 @@ diff-filenames() {
     [[ ${1-} == -s ]] &&
         { _s=1; shift; }
 
-    [[ $# -lt 2  || ${1-} == @(-h|--help) ]] &&
+    [[ $# -lt 2  || $1 == @(-h|--help) ]] &&
         { docsh -TD; return; }
 
-    # get basenames of unique filenames
-    # - %P : File's name with the search starting-point removed.
-    local uniq_bns
-    mapfile -t uniq_bns < \
-        <(  command find "$@" -printf '%P\n' \
+    [[ -d $1 ]] \
+        || { err_msg 3 "directory not found: '$1'"; return; }
+
+    [[ -d $2 ]] \
+        || { err_msg 3 "directory not found: '$2'"; return; }
+
+    # find command and search roots
+    local find_cmd pth1 pth2
+    pth1=$1
+    pth2=$2
+
+    find_cmd=( "$( builtin type -P find )" "$@" ) \
+        || return 9
+    shift $#
+
+    # get relative paths of unique filenames
+    # - %P : File's path with the search starting-point removed.
+    local uniq_fns
+    mapfile -t uniq_fns < \
+        <(  "${find_cmd[@]}" -printf '%P\n' \
                 | sort \
                 | uniq -u )
 
-    if [[ ${#uniq_bns[@]} -gt 0 ]]
+    if [[ ${#uniq_fns[@]} -gt 0 ]]
     then
         # add a find -path arg for each file
-        local i f_args=( '(' )
+        find_cmd+=( '(' )
 
-        for i in "${!uniq_bns[@]}"
+        local i
+        for i in "${!uniq_fns[@]}"
         do
             [[ $i -gt 0 ]] &&
-                f_args+=( '-o' )
+                find_cmd+=( '-o' )
 
-            f_args+=( -path "*/${uniq_bns[i]}" )
+            find_cmd+=( -path "${pth1}/${uniq_fns[i]}" -o -path "${pth2}/${uniq_fns[i]}" )
         done
 
-        f_args+=( ')' )
+        find_cmd+=( ')' )
 
-        command find "$@" "${f_args[@]}"
+        "${find_cmd[@]}"
 
     elif [[ -n ${_s-} ]]
     then
