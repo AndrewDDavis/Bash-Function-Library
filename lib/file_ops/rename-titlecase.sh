57d1570f3f5273be9cbb8260931f510e0f5a817a
Andrew Davis <addavis@gmail.com>
2025-05-07 14:21:22 -0400

    ignore short words; docs

--- lib/file_ops/rename-titlecase.sh
+++ lib/file_ops/rename-titlecase.sh
@@ -1,32 +1,63 @@
-import_func str_to_words \
+# dependencies
+import_func str_to_words array_match \
     || return 63
 
 rename-titlecase() {
 
-    : "Change filename words to title-case"
+    : "Change filename words to title-case
+
+        Usage: rename-titlecase {file-name} ...
+
+          - Operates only on file basenames, not the whole path.
+          - Ignores some small words, e.g. a, an, the, and, at, with, etc.
+    "
 
     [[ $# -eq 0  || $1 == @(-h|--help) ]] &&
         { docsh -TD; return; }
 
-    local bn dn bn_words obn ofn1 ofn2
+    # words to ignore
+    local ign_words=( a an the and yet if to at in or nor with )
 
-    for fn in "$@"
+    # loop over filenames
+    local ofn obn odn \
+        obn_words i wd \
+        nbn nfn1 nfn2
+
+    for ofn in "$@"
     do
-        bn=$( basename "$fn" )
-        dn=$( dirname "$fn" )
+        [[ -e $ofn ]] \
+            || { err_msg 3 "file not found: '$ofn'; aborting..."; return; }
 
-        # read filename words as array
-        # IFS=$'\n' read -ra bn_words -d '' < <( compgen -W "$bn" )
-        str_to_words bn_words "$bn"
+        obn=$( basename "$ofn" )
+        odn=$( dirname "$ofn" )
+
+        # split filename into array of words
+        # - alternative: IFS=$'\n' read -ra obn_words -d '' < <( compgen -W "$obn" )
+        str_to_words obn_words "$obn"
 
         # generate new basename, using uppercase operator
-        obn=${bn_words[*]@u}
+        # - this doesn't ignore articles: nbn=${obn_words[*]@u}
+        i=0
+        nbn=$obn
+        for wd in "${obn_words[@]}"
+        do
+            # ignore articles
+            array_match ign_words "$wd" \
+                && (( i > 0 )) \
+                && continue
+
+            nbn=${nbn/"$wd"/"${wd@u}"}
+            (( ++i ))
+        done
+
+        [[ $nbn == "$obn" ]] \
+            && continue
 
         # use a 2-step rename for case-insensitive filesystems (macOS)
-        ofn1="$dn/_rn_$obn"
-        ofn2="$dn/$obn"
+        nfn1="$odn/_rn_$nbn"
+        nfn2="$odn/$nbn"
 
-        command mv -vi "$fn" "$ofn1" \
-            && command mv -vi "$ofn1" "$ofn2"
+        command mv -vi "$ofn" "$nfn1" \
+            && command mv -vi "$nfn1" "$nfn2"
     done
 }
