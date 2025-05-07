b32b8b0e5506ebf401f570de01d6717242ca0179
Andrew Davis <addavis@gmail.com>
2025-05-07 14:36:18 -0400

    added -a; docs; code refactor

--- lib/shell/strings/str_split.sh
+++ lib/shell/strings/str_split.sh
@@ -4,34 +4,38 @@ str_split() {
 
         : "Split string into array elements
 
-        Usage: str_split [-d delim] <array-name> [string]
+        Usage: str_split [-d delim] <array-name> [string] ...
 
-        This function populates the elements of the array by splitting the supplied
-        string. By default, or when delim is the empty string, each element corresponds
-        to a single character from the string. If no string is supplied on the command
-        line, STDIN is used.
+        The elements of the indicated array are defined by splitting the supplied
+        string. By default, and when delim is the empty string, the string is split into
+        individual characters. If no string is supplied on the command line, STDIN is
+        used.
 
         Options
 
-          -d delim : split string at each instance of delim
-          -q       : suppress warning when array is not empty
+          -a       : append to array instead of clearing it
+          -d delim : split string at each instance of 'delim'
+          -q       : suppress warning when non-empty array is cleared
 
         Examples
 
           str_split str_elems 'each_char_is_an_elem'
           str_split -d '/' path_elems 'path/to/split'
+
+        See Also: str_to_words, str_join_with
         "
         docsh -TD
         return
     }
 
-    # verbosity
-    local _v=1 _d=''
+    # defaults
+    local _v=1 _d='' _a
 
     local flag OPTARG OPTIND=1
-    while getopts ':d:q' flag
+    while getopts ':ad:q' flag
     do
         case $flag in
+            ( a ) _a=1 ;;
             ( d ) _d=$OPTARG ;;
             ( q ) (( _v-- )) ;;
             ( \? ) err_msg 2 "unknown option: '-$OPTARG'"; return ;;
@@ -41,45 +45,59 @@ str_split() {
     shift $(( OPTIND-1 ))
 
     # nameref to the array variable
-    local -n _ss_arr=$1 || return
+    local -n _ss_arr=$1 \
+        || return
     shift
 
     # read stdin if necessary
-    # - don't be tempted to use $( < /dev/stdin ), as [noted](https://unix.stackexchange.com/a/716439/85414)
+    # - don't be tempted to use $( < /dev/stdin ),
+    #   as [noted](https://unix.stackexchange.com/a/716439/85414)
     [[ $# -eq 0 ]] &&
         set -- "$( cat )"
 
-    # clear the array
-    [[ $_v -gt 0 ]] && {
-        # - check array and warn
-        # - avoiding unbound variable on ${#_ss_arr[@]}
-        [[ -n ${_ss_arr[*]} ]] &&
-            err_msg w "clearing non-empty array '${!_ss_arr}'"
-    }
-    _ss_arr=()
+    # split string(s) into temp array
+    local s _tmp_arr=()
+    for s in "$@"
+    do
+        if [[ -z $_d ]]
+        then
+            # Split string into individual characters
+            local i
+            for (( i=0; i<${#s}; i++ ))
+            do
+                _tmp_arr+=( "${s:i:1}" )
+            done
 
+        elif [[ -n $( command -v mapfile ) ]]
+        then
+            # Use the mapfile builtin (AKA readarray in newer Bash)
+            mapfile -td "$_d" _tmp_arr < \
+                <( printf '%s' "$s" )
 
-    # Safely populate the array
-    if [[ -z $_d ]]
+        else
+            # Use read in classic Bash
+            local elem
+            while IFS='' read -rd "$_d" elem
+            do
+                _tmp_arr+=( "$elem" )
+            done < \
+                <( [[ "${s:(-1)}" == "$_d" ]] && printf '%s' "$s" || printf "%s$_d" "$s" )
+        fi
+    done
+    shift $#
+
+    # replace or append to array
+    if [[ -v _a ]]
     then
-        # Split string into individual characters
-        local -i i
-        for (( i=0; i<${#1}; i++ ))
-        do
-            _ss_arr+=( "${1:i:1}" )
-        done
-
-    elif [[ -n $( command -v mapfile ) ]]
-    then
-        # Use the mapfile builtin (AKA readarray in newer Bash)
-        # - Nulling IFS is necessary to prevent stripping of leading and trailing
-        #   whitespace in the fields.
-        IFS='' mapfile -td "$_d" _ss_arr < \
-            <( printf '%s' "$1" )
+        _ss_arr+=( "${_tmp_arr[@]}" )
 
     else
-        # Use read in classic Bash
-        IFS="$_d" read -r -d '' -a _ss_arr < \
-            <( printf '%s\0' "$1" )
+        # clear the array
+        (( _v > 0 )) && {
+            # - this check is better than ${#_ss_arr[@]} and [[ -n ${_ss_arr[*]} ]]
+            [[ -v '_ss_arr[*]' ]] \
+                && err_msg w "clearing non-empty array '${!_ss_arr}'"
+        }
+        _ss_arr=( "${_tmp_arr[@]}" )
     fi
 }
