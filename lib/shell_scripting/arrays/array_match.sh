d4bb97a8355b418eb9a63d617417a1fc543797ac
Andrew Davis <addavis@gmail.com>
2025-05-15 23:56:15 -0400

    docs; code refactor to simplify; add -c and -a

--- lib/shell_scripting/arrays/array_match.sh
+++ lib/shell_scripting/arrays/array_match.sh
@@ -10,22 +10,29 @@ array_match () {
 
         Usage: array_match [options ...] <array-name> <pattern>
 
-        The 'array-name' argument is the name of an existing array variable; don't pass
-        the whole expanded array.
+        The elements of the array are tested against the pattern. By default,
+        array_match returns silently with a true or false return status. For the
+        'array-name' argument, pass the name of an existing array variable, not the
+        expanded array.
 
-        By default, matching uses Posix ERE regular expression syntax to match an entire
-        array element, rather than a substring.
+        By default, the pattern is treated as a POSIX ERE that must match an entire
+        array element, rather than a substring. For the pattern syntax, refer to the
+        manpages of 'regex(7)' and 'grep'.
 
         Options
 
-        -E : pattern is Posix ERE regex
-        -F : pattern is a fixed string
+        -E : treat pattern as POSIX extended regular expression (ERE)
+        -F : treat pattern as a fixed string
+
         -i : case-insensitive match
+        -s : allow substring match within array elements
+        -v : invert the logic: test for elements that do not match the pattern
+
+        -c : print only the count of matching elements (overrides -n and -p)
         -n : print the index or key of the first matching array element
         -p : print the value of the first matching array element. If both -n and -p are
              used, the output is in the form 'key:value'.
-        -s : allow substring match within array elements
-        -v : invert the logic: test for elements that do not match the pattern
+        -a : with -n or -p, print all elements or keys, not just the first
 
         The return status is 0 (true) for a match, 1 (false) for no match, or > 1 if an
         error occurs. An empty array always returns 1. It is an error if the named
@@ -33,31 +40,35 @@ array_match () {
 
         Examples
 
-        arr=('one' 'bananas' 'apples' 'two words')
+          arr=('one' 'bananas' 'apples' 'two words')
 
-        # returns true:
-        array_match arr bananas
+          # returns true:
+          array_match arr bananas
 
-        # returns false:
-        array_match arr banana
+          # returns false:
+          array_match arr banana
 
-        # returns true:
-        array_match -s arr banana
+          # returns true:
+          array_match -s arr banana
 
-        # returns true:
-        array_match arr 'ban.*'
+          # returns true:
+          array_match arr 'ban.*'
 
-        # returns false:
-        array_match -F arr 'ban.*'
+          # returns false:
+          array_match -F arr 'ban.*'
 
-        # returns true and prints the element index:
-        array_match -n arr 'ban.*'
-        # 2
+          # prints the first matching element index:
+          array_match -n arr 'ban.*'
+          # 1
 
-        # returns true and prints the index and the matching element:
-        array_match -np arr 'ban.*'
-        # 2
-        # bananas
+          # prints all matching indices and elements:
+          array_match -anp arr '.*n.*'
+          # 0:one
+          # 1:bananas
+
+          # number of elements with an s
+          array_match -sc arr 's'
+          # 3
         "
         docsh -TD
         return
@@ -70,128 +81,122 @@ array_match () {
     ' RETURN
 
     # defaults and arg-parsing
-    local re_type=E _i _n _p _x=x _v _verb=1
+    local re_type=E \
+        _a _i _x=x _v \
+        _c _n _p
 
     local flag OPTARG OPTIND=1
-    while getopts "EFinpsv" flag
+    while getopts "EFaisvcnp" flag
     do
         case $flag in
-            ( E | F )
-                re_type=$flag ;;
-            ( i )
-                _i=i ;;
-            ( n )
-                _n=1 ;;
-            ( p )
-                _p=1 ;;
-            ( s )
-                unset _x ;;
-            ( v )
-                _v=v ;;
+            ( E | F ) re_type=$flag ;;
+            ( a )  _a=1  ;;
+            ( i )  _i=i  ;;
+            ( s )  _x='' ;;
+            ( v )  _v=v  ;;
+            ( c )  _c=1  ;;
+            ( n )  _n=1  ;;
+            ( p )  _p=1  ;;
             ( \? )
                 err_msg 2 "illegal option: '$OPTARG'"
                 return ;;
         esac
     done
-    shift $(( OPTIND - 1 ))
-
+    shift $(( OPTIND-1 ))
 
     # positional args
-    [[ $# -eq 2 ]] || return 2
+    [[ $# -eq 2 ]] \
+        || return 2
 
     # - array nameref and pattern (or string)
-    local -n arrnm=$1   || return
-    local ptn=$2        || return
+    local -n __arrnm=$1     || return
+    local ptn=$2            || return
     shift 2
 
+    # check for valid, non-empty array
+    # - NB, this returns n=0 for empty array, or false for scalar variable or unset var
+    # - OTOH, [[ -v __arrnm[*] ]] returns false for empty array (referenced or not), or unset var
+    n=${#__arrnm[*]} \
+        || { err_msg 3 "not an array: '${!__arrnm}'"; return; }
 
-    # check valid name and non-empty array
-    # - NB, [[ -v arrnm[*] ]] returns false for empty array (referenced or not), or unset var
-    # - n=0 for empty array, returns false for scalar variable, or unset var
-    n=${#arrnm[*]} \
-        || { err_msg 3 "not an array: '${!arrnm}'"; return; }
+    (( n == 0 )) \
+        && return 1
 
-    if (( n == 0 ))
-    then
-        # empty array, no matches
-        return 1
-    fi
 
     # Both matching styles below use grep
-    #
-    # - I originally tried a form using Bash '[[': [[ "${arrnm[@]}" =~ $ptn ]]. This was
-    #   very concise, but not totally precise: an expression combining two adjascent
-    #   array values with a space between matches, even though it should not.
-    #
-    # Relevant grep options:
-    #   -F : pattern is a fixed string
-    #   -E : pattern is an ERE
-    #   -q : quiet (also quits immediately on match, which helps efficiency)
-    #   -x : match whole lines
-    #   -z : null-terminated lines
-    #   -m <n> : stop after finding <n> matches
-    #   -n : prefix each output line with the 1-based line number (e.g. '53:...')
-    local grep_opts+=( -q${re_type}${_x-}${_i-}${_v-} )
+    # - NB, I originally tried a form using Bash '[[': [[ "${__arrnm[@]}" =~ $ptn ]].
+    #   This was very concise, but not totally precise: an expression combining two
+    #   adjascent array values with a space between matches, even though it should not.
+    local grep_cmd grep_out
+    grep_cmd=( "$( builtin type -P grep )" ) \
+        || return
 
     # grep returns with status 0 for a match, 1 for no match
-    local grep_rs=1
+    # local -i grep_rs=0
 
-    _grep_run() {
-        (
-            grep_cmd=$( builtin type -P grep )
-            [[ $_verb -gt 1 ]] && set -x
-            "$grep_cmd" "${grep_opts[@]}" -e "$ptn"
-        )
-        grep_rs=$?
-    }
+    # relevant grep options:
+    #   -E : pattern is an ERE
+    #   -F : pattern is a fixed string
+    #   -i : case-insensitive
+    #   -m <n> : stop after finding <n> matches
+    #   -n : prefix each output line with the 1-based line number (e.g. '53:...')
+    #   -q : quiet (also quits immediately on match, which helps efficiency)
+    #   -v : invert match logic
+    #   -x : match whole lines
+    #   -z : null-terminated lines
+    grep_cmd+=( -nz "-${re_type}${_i-}${_v-}${_x-}" -e "$ptn" )
 
-    if [[ -z ${_n-}  &&  -z ${_p-} ]]
+    # if not all or count, then only 1 and done
+    [[ -v _a  || -v _c ]] \
+        || grep_cmd+=( -m1 )
+
+    # create array of keys
+    # - needed for sparse or associative array
+    # - this locks in an order for associative arrays
+    # - the index of this array matches the line numbers output by grep
+    # - they values are the indices of the input array
+    local i keys=( '' "${!__arrnm[@]}" )
+
+    # now we can run with -n, and later convert the line number to the array index
+    mapfile -d '' grep_out < \
+        <( "${grep_cmd[@]}" < \
+            <( for i in "${keys[@]:1}"; do printf '%s\0' "${__arrnm[i]}"; done ) )
+
+    # grep return status
+    # - NB, $! expands to PID of most recent background job
+    # - grep returns with status 0 for a match, 1 for no match
+    wait $! \
+        || return
+
+    # now grep_out has e.g., grep_out=([0]="1:abc" [1]="4:bbb")
+    # - it will only have one entry, unless -a or -c were used
+
+    # key for which info to print
+    local k=${_n:-0}${_p:-0}
+
+    if [[ -v _c ]]
     then
-        # Just return binary answer
+        # match count
+        printf '%s\n' "${#grep_out[@]}"
+        return
 
-        # use null-terminated strings
-        grep_opts+=( -z )
-
-        # - NB, adding '-n -m1' to capture the line number and use it as the index would
-        #   sometimes work, but not for associative arrays, or indexed arrays with
-        #   non-contiguous indexes. Also, grep keeps the null from the input, rather
-        #   than removing it, so you would need to strip it or convert it to a newline
-        #   if capturing output (e.g. with tr).
-
-        _grep_run < <( printf '%s\0' "${arrnm[@]}" )
+    elif [[ $k == 00 ]]
+    then
+        # not printing, just use return status
+        true
 
     else
-        # Print matching element and/or its index (or key)
-        # - use a loop to work for all arrays (associative, non-contig indexed)
-
-        local key
-
-        for key in "${!arrnm[@]}"
+        # convert line numbers to array index
+        local s
+        for s in "${grep_out[@]}"
         do
-            # debug
-            [[ $_verb -gt 2 ]] &&
-                printf >&2 ':key:%s::arrnm[key]:%s:\n' "$key" "${arrnm[$key]}"
+            i=${s%%:*}
 
-            _grep_run <<< "${arrnm[$key]}"
-
-            if [[ $grep_rs -eq 0 ]]
-            then
-                if [[ -n ${_n-}  &&  -n ${_p-} ]]
-                then
-                    printf '%s:%s\n' "$key" "${arrnm[$key]}"
-
-                elif [[ -n ${_n-} ]]
-                then
-                    printf '%s\n' "$key"
-
-                elif [[ -n ${_p-} ]]
-                then
-                    printf '%s\n' "${arrnm[$key]}"
-                fi
-                break
-            fi
+            case $k in
+                ( 11 ) printf '%s:%s\n' "${keys[i]}" "${s#*:}" ;;
+                ( 10 ) printf '%s\n' "${keys[i]}" ;;
+                ( 01 ) printf '%s\n' "${s#*:}" ;;
+            esac
         done
     fi
-
-    return $grep_rs
 }
