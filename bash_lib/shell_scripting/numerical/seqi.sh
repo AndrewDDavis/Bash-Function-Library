9bcf13bd99d49e69b1ce177c04fac568e82e5018
Andrew Davis <addavis@gmail.com>
2025-05-24 00:04:00 -0400

    speed up by splitting integer generation and string production

--- bash_lib/shell_scripting/numerical/seqi.sh
+++ bash_lib/shell_scripting/numerical/seqi.sh
@@ -17,17 +17,21 @@ seqi() {
 
           - If last is less than first, and increment is omitted, the increment is -1.
 
+        In terms of time, seqi operates faster than seq for small numbers of values,
+        since seq is an external binary. However, seq is faster above ~100 values.
+
         Options
 
         -s <string>
-        : separate printed numbers using this string (default: newline)
+        : separate printed numbers using this format string
+          (default: '\n', representing a newline)
 
         -w
         : equalize width of printed numbers by padding with leading zeroes
     "
 
     # defaults and options
-    local _s=$'\n' _w
+    local _s='\n' _w
 
     local flag n OPTARG OPTIND=1
     while getopts ':s:wh' flag
@@ -38,16 +42,16 @@ seqi() {
             ( h ) docsh -TD; return ;;
             ( \? )
                 [[ $OPTARG == [0-9] ]] && {
-                    # consider 'first' args of -2 or -16, which look like option flags
+                    # handle 'first' positional args of -2 or -16, which look like option flags
                     n=$(( OPTIND-1 ))
                     [[ ${!n} == -[0-9] ]] \
                         && (( OPTIND-- ))
                     break
                 }
-                err_msg 3 "unknown option: '-$OPTARG'"
+                err_msg 3 "unknown option: '$OPTARG'"
                 return
             ;;
-            ( : ) err_msg 4 "missing argument for -$OPTARG"; return ;;
+            ( : ) err_msg 4 "missing argument for $OPTARG"; return ;;
         esac
     done
     shift $(( OPTIND-1 ))
@@ -63,12 +67,11 @@ seqi() {
     esac
     shift $#
 
-    # ensure incr has a value
+    # ensure valid incr
     if [[ -z ${incr-} ]]
     then
         incr=1
-        (( frst > last )) \
-            && incr=-1
+        (( frst > last )) && incr=-1
 
     elif (( incr == 0 ))
     then
@@ -76,41 +79,48 @@ seqi() {
         return
     fi
 
-    # longest string could be first or last, e.g. -2 vs 3
-    # - 'seq -w -2 3' prints -2 -1 00 01 02 03
-    local slen=${#frst}
-    (( ${#last} > ${#frst} )) \
-        && slen=${#last}
-
-    # set width format string
-    # e.g. printf '%03d\n' 3
-    local fmt=''
-    [[ -v _w ]] \
-        && fmt=0${slen}
-
-    fmt="%${fmt}d"
-
-    # define the comparison
-    local comp
-    if (( incr > 0 ))
+    # output format
+    local fmt="%d${_s}"
+    if [[ -v _w ]]
     then
-        comp='i<=last'
-    else
-        comp='i>=last'
+        # set width format string
+        # e.g. printf '%03d\n' 3
+
+        # longest string could be first or last, e.g. -2 vs 3
+        # - 'seq -w -2 3' prints -2 -1 00 01 02 03
+        local slen=${#frst}
+        (( ${#last} > ${#frst} )) \
+            && slen=${#last}
+
+        fmt="%0${slen}d${_s}"
     fi
 
-    # create string of formatted integers
-    local i ostr=''
+    # define the comparison and integer list
+    local comp='i<=last'
+    (( incr < 0 )) && comp='i>=last'
+
+    local i is=()
     for (( i=frst; "$comp"; i=i+incr ))
     do
-        ostr+=$( printf "$fmt" "$i" )
-        ostr+=$_s
+        is+=( "$i" )
     done
 
-    # strip the last separator and print the string with a newline
-    if [[ -n $ostr ]]
+    # - generating the whole string in one step sped this up
+    #   from ~900 ms for 1000 entries, to ~15 ms
+    # - for small numbers of entries, generating the string still takes a large amount
+    #   of the total time, whereas the above loop to generate the array takes < 1 ms
+    # - above ~ 1000 entries, the above loop takes more time than generating the string
+    # - for ~ 5000 entries, the print statement takes about the same time as generating
+    #   the string
+
+    if [[ -v is[*] ]]
     then
-        ostr=${ostr%"$_s"}
-        printf '%s\n' "$ostr"
+        # generate formatted string
+        # - NB, empty is supplies 0 as number
+        local ostr
+        ostr=$( printf "$fmt" "${is[@]}" )
+
+        # strip the last separator and print the string with a newline
+        printf '%s\n' "${ostr%"$(printf "$_s")"}"
     fi
 }
