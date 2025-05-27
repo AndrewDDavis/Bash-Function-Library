afc6e73d9f78f8fe124ac8fd9e8c56fe5c80f485
Andrew Davis <addavis@gmail.com>
2025-05-27 16:37:36 -0400

    modularize code; inherit verbosity

--- bash_lib/shell_scripting/run_vrb.sh
+++ bash_lib/shell_scripting/run_vrb.sh
@@ -1,24 +1,29 @@
-# deps
+# dependencies
 import_func is_int \
     || return
 
 run_vrb() {
 
-    [[ $# -eq 0  ||  $1 == @(-h|--help) ]] && {
+    [[ $# -eq 0  || $1 == @(-h|--help) ]] && {
 
         : "Print command line to STDERR as it is being run
 
         Usage: run_vrb [options] [--] <command-line ...>
 
-        This function runs the command line with the xtrace (set -x) option enabled.
-        This causes the command line to be printed on STDERR as it is executed,
-        prepended by the \$PS4 string (currently '$( sed -E 's/\\(\[|\])//g' <<< $PS4 )').
+        This function runs the command line provided. If the verbosity setting is high
+        enough, the command line is printed on STDERR as it is executed, prepended by
+        the \$PS4 string (usually '+ '). This is accomplished by temporarily enabling
+        the xtrace (set -x) shell option.
+
+        The verbosity setting is established by looking for a shell variable called '_v'
+        or '_verb' with a numerical value. If no such variable is found, the default
+        value is 2, which may be changed using the -v option. The verbosity threshold
+        is 1, above which the command line is printed.
 
         Options
 
           -v <n>
-          : n is an integer indicating the verbosity level. If n > 1, the command line
-            is printed. The default value is 2.
+          : set verbosity level to integer value n.
 
           -P
           : resolve the command name to either a shell builtin or file on the PATH, and
@@ -27,8 +32,11 @@ run_vrb() {
 
         If environment variable settings in the form of A=B are used at the start of the
         command line, the env command is prepended to ensure the command runs. Note that
-        env is incompatible with running shell functions or builtins, so a command line
-        like 'A=B run_vrb command args' is recommended.
+        env is incompatible with running shell functions or builtins, so an alternative
+        command line in the form of 'A=B run_vrb command args' is recommended.
+
+        If the command is a shell function, not only the function call is printed to
+        STDERR, but also the commands within the function.
 
         The return status code is usually the return status of the command line, but may
         be 2 if there was an option problem, or 124-6 if there was a problem determining
@@ -46,100 +54,141 @@ run_vrb() {
         return
     }
 
-    ## Defaults and options
-    # - NB, getopts breaks the loop on '--' and advances OPTIND
-    local _v=2 setx _P
+    # cleanup routine
+    trap '
+        unset -f _parse_opts _parse_posargs _rslv_cmd
+        trap - return
+    ' RETURN
 
-    local flag OPTARG OPTIND=1
-    while getopts ':v:P' flag
-    do
-        case $flag in
-        ( v )
-            is_int "$OPTARG" \
-                || return
-            _v=$OPTARG
-        ;;
-        ( P ) _P=1 ;;
-        ( \? ) err_msg 2 "unknown option: '$OPTARG'"; return ;;
-        ( : ) err_msg 2 "missing argument for '$OPTARG'"; return ;;
-        esac
-    done
-    shift $(( OPTIND-1 ))
+    _parse_opts() {
 
-
-    # check for set -x in the current shell
-    (( _v > 1 )) && [[ $- != *x* ]] \
-        && setx=1
-
-
-    ## Further arguments form the command line to be executed
-    # - split it into A=B args, the command name, and command args
-    # - NB, I used to use array_match() in here, but that caused a function loop (!)
-    local i _env eargs=() \
-        cmd=() args=()
-
-    for (( i=1; i<=$#; i++ ))
-    do
-        if [[ $i -eq 1  &&  ${!i} == @(env|*/env) ]]
+        # check for inherited verbosity
+        if [[ ! -v _verb ]]
         then
-            # env command
-            _env=1
-            eargs+=( "${!i}" )
-
-        elif [[ ${!i} == *=* ]]
-        then
-            # env-var setting
-            eargs+=( "${!i}" )
-
-        else
-            # command, followed by args
-            cmd[0]=${!i}
-            (( i++ ))
-            args+=( "${@:i}" )
-            shift $#
-            break
+            if [[ -v _v ]]
+            then
+                _verb=$_v
+            else
+                _verb=2
+            fi
         fi
-    done
 
-    [[ -v cmd[*] ]] \
-        || return 124
+        # parse options
+        # - NB, getopts breaks the loop on '--' and advances OPTIND
+        local flag OPTARG OPTIND=1
+        while getopts ':v:P' flag
+        do
+            case $flag in
+                ( v ) _verb=$OPTARG ;;
+                ( P ) _P=1 ;;
+                ( \? ) err_msg 2 "unknown option: '$OPTARG'"; return ;;
+                ( : )  err_msg 3 "missing argument for '$OPTARG'"; return ;;
+            esac
+        done
+        n=$(( OPTIND-1 ))
 
+        is_int "$_verb" \
+            || { err_msg 2 "non-integer verbosity: '$_verb'"; return; }
+    }
 
-    # if A=B env var settings are present, the env command is needed
-    [[ ${#eargs[*]} -gt 0  && ! -v _env ]] \
-        && eargs=( "$( builtin type -P env )" "${eargs[@]}" )
+    _parse_posargs() {
 
+        # categorize the words of the command-line into A=B args, the command name,
+        # and command args
+        # - NB, I used to use array_match() in here, but that caused a function loop (!)
 
-    if [[ -v _P ]]
-    then
+        # detect env command (must be first)
+        if [[ $1 == @(env|*/env) ]]
+        then
+            env_args+=( "$1" )
+            shift
+        fi
+
+        while [[ -v 1 ]]
+        do
+            if [[ $1 == *=* ]]
+            then
+                # env-var setting
+                # - ensure env command
+                [[ -v env_args[*] ]] \
+                    || env_args+=( "$( builtin type -P env )" )
+
+                env_args+=( "$1" )
+
+            else
+                # command, followed by args
+                cmd_args=( "$@" )
+                shift $#
+                break
+            fi
+            shift
+        done
+
+        [[ -n ${cmd_args[*]} ]] \
+            || { err_msg 124 "no command detected"; return; }
+    }
+
+    _rslv_cmd() {
+
         # resolve command to builtin or file
         # - NB, 'type -at' returns multi-line string containing alias, keyword,
         #   function, builtin, file or ''
         local types
-        types=$( builtin type -at "${cmd[0]}" ) \
-            || return 125
+        types=$( builtin type -at "${cmd_args[0]}" ) \
+            || {
+            err_msg 125 "type failure for command: '${cmd_args[0]}'"
+            return
+        }
 
         # adjust command argument
-        if [[ $types == *builtin* ]]
-        then
-            cmd=( builtin "${cmd[0]}" )
+        case $types in
+            ( *builtin* )
+                [[ -z ${env_args[*]} ]] \
+                    || { err_msg 9 "builtins (${cmd_args[0]}) not compatible with env"; return; }
 
-        elif [[ $types == *file* ]]
-        then
-            cmd[0]=$( builtin type -P "${cmd[0]}" )
+                [[ ${cmd_args[0]} == builtin ]] \
+                    || cmd_args=( builtin "${cmd_args[@]}" )
+            ;;
+            ( *file* )
+                # NB, if cmd is a path already, type -P just gives it back
+                cmd_args[0]=$( builtin type -P "${cmd_args[0]}" )
+            ;;
+            ( * )
+                err_msg 126 "command is not a builtin or file: '${cmd_args[0]}'"
+                return
+            ;;
+        esac
+    }
 
-        else
-            return 126
-        fi
-    fi
+    # define options and verbosity setting
+    local -I _verb
+    local -i n _P
+    _parse_opts "$@" || return
+    shift $n
 
+    # parse positional args and define the command line to be executed
+    local env_args=() cmd_args=()
+    _parse_posargs "$@" || return
+    shift $#
+
+    # resolve command name if indicated
+    [[ -v _P ]] && { _rslv_cmd || return; }
+
+    # enable xtrace if indicated, and not already set
+    local setx
+    [[ _verb -gt 1  && $- != *x* ]] && {
+        setx=1
+        set -x
+    }
 
     # run command line
-    [[ -v setx ]] \
-        && set -x
+    "${env_args[@]}" "${cmd_args[@]}"
 
-    "${eargs[@]}" "${cmd[@]}" "${args[@]}"
+    {
+        n=$?
+        [[ -v setx ]] \
+            && set +x
 
-    { [[ -v setx ]] \
-        && set +x; } 2>/dev/null
+    } 2>/dev/null
+    return $n
 }
