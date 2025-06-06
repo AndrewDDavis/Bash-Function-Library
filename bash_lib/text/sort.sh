4263f3c715a16c0cc81bfee77e8e292c48cf0bf2
Andrew Davis <addavis@gmail.com>
2025-06-06 14:06:35 -0400

    bugfix and docs

--- bash_lib/text/sort.sh
+++ bash_lib/text/sort.sh
@@ -1,24 +1,36 @@
-# Sort command
-
 sort-noansi() {
 
     [[ $# -gt 0  && $1 == @(-h|--help) ]] && {
 
-        : """Sort while ignoring ANSI escape sequences
+        : """Sort a list while ignoring ANSI escape sequences
 
-          - Uses awk to sort the text provided on STDIN.
-          - Any options provided are passed to sort.
+            Usage: sort-noansi [sort-opts] <<< \"input\"
+
+            Awk is used to make a duplicate of the first field of each input line on
+            STDIN, with any ANSI bits stripped out. The fields are created by splitting
+            a line by runs of whitespace, with leading and trailing whitespace ignored
+            (regex /[ \t\n]+/).
+
+            Then the sort command is used to sort the text, using any options provided.
+
+            Finally, the cut command is used to strip out the added field and return the
+            sorted lines.
         """
         docsh -TD
         return
     }
 
     # Use awk and sort
-    # - the first awk command duplicates the first field and strips out the ANSI bits
-    # - the last part strips out that added field
     # - from https://unix.stackexchange.com/a/157971/85414
+    local _filt='
+        {
+            s = $1
+            gsub(/\033\[[ -?]*[@-~]/, "", s)
+            print s "\t" $0
+        }
+    '
 
-    awk '{s=$0; gsub(/\033\[[ -?]*[@-~]/,"",s); print s "\t" $0}' \
+    awk "$_filt" \
         | sort "$@" \
         | cut -f 2-
 }
