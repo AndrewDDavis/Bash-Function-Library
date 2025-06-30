9f79b74ef8c82abdf09c76fe5db8012f87abcb8d
Andrew Davis <addavis@gmail.com>
2025-06-30 00:32:17 -0400

    new docs style

--- bash_lib/shell_scripting/arrays/array_strrepl.sh
+++ bash_lib/shell_scripting/arrays/array_strrepl.sh
@@ -2,22 +2,20 @@
 import_func is_set_array \
     || return
 
+: """Replace array element matching string with one or more new elements
+
+    Usage: array_strrepl <array-name> <string> [elem1 [elem2 ...]]
+
+    The first array element that exactly matches the string is replaced by elem1,
+    then any further elements are added to follow. If no new elements are provided,
+    the matching element is deleted. Indices following the replaced element
+    are adjusted as explained by array_irepl().
+"""
+
 array_strrepl() {
 
-    [[ $# -eq 0  ||  $1 == @(-h|--help) ]] && {
-
-        : """Replace array element matching string with one or more new elements
-
-        Usage: array_strrepl <array-name> <string> [elem1 [elem2 ...]]
-
-        The first array element that exactly matches the string is replaced by elem1,
-        then any further elements are added to follow. If no new elements are provided,
-        the matching element is deleted. Indices following the replaced element
-        are adjusted as explained by array_irepl().
-        """
-        docsh -TD
-        return
-    }
+    [[ $# -eq 0  ||  $1 == @(-h|--help) ]] &&
+        { docsh -TD; return; }
 
     # array name and pattern, then remaining args are new elements
     local -n __asr_arrnm__=$1   || return
@@ -47,7 +45,7 @@ array_strrepl() {
         # associative array
         if [[ $# -eq 0 ]]
         then
-            unset __asr_arrnm__["$k"]
+            unset "__asr_arrnm__[$k]"
 
         elif [[ $# -eq 1 ]]
         then
@@ -57,6 +55,7 @@ array_strrepl() {
             err_msg 5 "associative arrays can only take one element"
             return
         fi
+
     else
         # pass the original array name, to save a layer of abstraction
         array_irepl "${!__asr_arrnm__}" "$k" "$@"
