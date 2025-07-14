cd6b0a8598e1efef369b62d874b33752fd9efb3c
Andrew Davis <addavis@gmail.com>
2025-07-14 19:10:53 -0400

    - new docsh style - refactoring to remove errtrace - other cleanup

--- bash_lib/file_ops/cp-bak.sh
+++ bash_lib/file_ops/cp-bak.sh
@@ -1,94 +1,105 @@
+# dependencies
+import_func vrb_msg \
+    || return
+
 # mkbak may be more mnemonic
 alias mkbak="cp-bak"
 
+: """Create simple file backup copies
+
+    Usage : cp-bak [opts] [--] [cp-opts] <filename> ...
+
+    Options (unrecognized options are passed on to cp):
+
+      -d <dir>
+      : create copy in dir, rather than next to input file
+
+      -e <.foo>
+      : use extension .foo (or e.g. '~') instead of .bak
+
+      -o
+      : use .old extension instead of .bak
+
+      -O
+      : use .orig extension instead of .bak
+
+      -q
+      : omit verbose copy output
+
+      -v
+      : show result of copy using 'ls -l' (-vv to also show verbose copy op)
+
+    cp-bak makes a copy of each filename, creating a unique file name by
+    appending .bak and possibly an integer, as necessary. The copy is performed
+    using 'cp -pi' by default, to preserve permissons and modification times, and
+    not clobber existing files.
+
+    A warning is printed if the file copy differs from the original in permissions
+    or ownership. Since cp dereferences symlinks by default, this check does too.
+
+    This function is intended for simple file backups, e.g. when editing config
+    files, to allow quick reversion of changes. For a full incremental backup
+    solution, use e.g. borg-go or tar.
+"""
+
 cp-bak() (
 
-    [[ $# -eq 0 || $1 == @(-h|--help) ]] && {
+    [[ $# -eq 0  || $1 == @(-h|--help) ]] \
+        && { docsh -TD; return; }
 
-        docsh -TD """Create simple file backup copies
+    # return and errors and clean up on return
+    trap '
+        return
+    ' ERR
 
-        Usage : cp-bak [opts] [--] [cp-opts] <filename> ...
+    trap '
+        unset -f _parse_opts _show_stats _chk_stats
+        trap - err return
+    ' RETURN
 
-        Options (unrecognized options are passed on to cp):
+    _parse_opts() {
 
-          -d <dir>
-          : create copy in dir, rather than next to input file
+        local flag OPTARG OPTIND=1
+        while getopts ":d:e:oOqv" flag
+        do
+            case $flag in
+                ( d ) odir=${OPTARG%/} ;;
+                ( e ) bk_ext=$OPTARG ;;
+                ( o ) bk_ext=.old ;;
+                ( O ) bk_ext=.orig ;;
+                ( q ) (( _verb-- )) ;;
+                ( v ) (( _verb++ )) ;;
+                ( \? ) cp_opts+=( -"$OPTARG" ) ;;
+                ( : ) err_msg 3 "missing arg for $OPTARG"; return ;;
+            esac
+        done
+        n=$(( OPTIND-1 ))
 
-          -e <.foo>
-          : use extension .foo (or e.g. '~') instead of .bak
-
-          -o
-          : use .old extension instead of .bak
-
-          -O
-          : use .orig extension instead of .bak
-
-          -q
-          : omit verbose copy output
-
-          -v
-          : show result of copy using 'ls -l' (-vv to also show verbose copy op)
-
-        cp-bak makes a copy of each filename, creating a unique file name by
-        appending .bak and possibly an integer, as necessary. The copy is performed
-        using 'cp -pi' by default, to preserve permissons and modification times, and
-        not clobber existing files.
-
-        A warning is printed if the file copy differs from the original in permissions
-        or ownership. Since cp dereferences symlinks by default, this check does too.
-
-        This function is intended for simple file backups, e.g. when editing config
-        files, to allow quick reversion of changes. For a full incremental backup
-        solution, use e.g. borg-go or tar.
-        """
-        return 0
+        # verbose copy operation
+        if (( _verb == 1 || _verb > 2 ))
+        then
+            cp_opts+=( -v )
+        fi
     }
 
-    # clean up on return
-    trap 'unset -f _show_stats _chk_stats
-          trap - RETURN ERR' RETURN
-
-    # return on errors
-    trap 'trap-err $?
-          return' ERR
-
-    # extend traps to sub-functions
-    shopt -os errtrace    # functions inherit the ERR trap
-    #shopt -os functrace   # functions inherit RETURN and DEBUG traps
-
-    # args
-    local odir bk_ext=.bak vrb=1 cp_opts=( -pi )
-
-    local OPT OPTARG OPTIND=1
-
-    while getopts ":d:e:oOqv" OPT
-    do
-        case $OPT in
-            ( d ) odir=${OPTARG%/} ;;
-            ( e ) bk_ext=$OPTARG ;;
-            ( o ) bk_ext=.old ;;
-            ( O ) bk_ext=.orig ;;
-            ( q ) vrb=$(( vrb - 1 )) ;;
-            ( v ) vrb=$(( vrb + 1 )) ;;
-            ( \? ) cp_opts+=( -"$OPTARG" ) ;;
-        esac
-    done
-    shift $(( OPTIND - 1 ))
-
-    # verbose copy operation
-    [[ $vrb -eq 1  ||  $vrb -gt 2 ]] &&
-        cp_opts+=( -v )
-
     _show_stats() {
+
+        trap 'return' ERR
+        trap 'trap - err return' RETURN
+
         # print mode (octal), user and group IDs in expected format
         # args: filename
         stat -L --format='%04a %u:%g' "$1"
     }
 
     _chk_stats() {
+
         # check for consistent metadata
+        trap 'return' ERR
+        trap 'trap - err return' RETURN
 
         # keep local as a separate cmd, to retain return status
+        local -i l
         local l _msg f1_stats f2_stats
 
         # don't use proc subst, so we retain the return status
@@ -99,11 +110,11 @@ cp-bak() (
 
         # minimum filename string length
         l=8
-        [[ ${#1} -gt $l ]] && l=${#1}
-        [[ ${#2} -gt $l ]] && l=${#2}
+        (( ${#1} > l )) && l=${#1}
+        (( ${#2} > l )) && l=${#2}
 
         # mode
-        if [[ ${f1_stats[0]} != ${f2_stats[0]} ]]
+        if [[ ${f1_stats[0]} != "${f2_stats[0]}" ]]
         then
             # show a warning message for differences
             printf -v _msg "%-${l}s  %6s\n" \
@@ -117,7 +128,7 @@ cp-bak() (
         fi
 
         # ownership
-        if [[ ${f1_stats[1]} != ${f2_stats[1]} ]]
+        if [[ ${f1_stats[1]} != "${f2_stats[1]}" ]]
         then
             printf -v _msg "%-${l}s  %12s\n" \
                 "filename" "owner:group" \
@@ -130,17 +141,28 @@ cp-bak() (
         fi
     }
 
+    # defaults and args
+    local odir \
+        bk_ext=.bak \
+        cp_opts=( -pi ) \
+        _verb=1
+
+    # parse options
+    local n
+    _parse_opts "$@"
+    shift "$n"
 
     for fn in "$@"
     do
         [[ -d $fn ]] && {
-            echo >&2 "Warning: directories not supported (try tar); skipping ${fn}"
+            err_msg w "dirs not supported (try tar); skipping ${fn}"
             continue
         }
 
         # split fn
-        local fbn=$( basename "$fn" )
-        local fdn=$( dirname "$fn" )
+        local fbn fdn
+        fbn=$( basename "$fn" )
+        fdn=$( dirname "$fn" )
 
         # add ./ for relative paths, for consistency of later reporting
         [[ $fdn == '.'  &&  ${fn:0:2} != './' ]] &&
@@ -158,16 +180,17 @@ cp-bak() (
         bk_fn=${odir}/${fbn}${bk_ext}
 
         # add integer as necessary
-        i=1
+        local -i i=1
         while [[ -e ${bk_fn} ]]
         do
-            let 'i += 1'
+            (( i++ ))
             bk_fn=${odir}/${fbn}${bk_ext}${i}
 
-            (( i == 100 )) && {
+            if (( i > 99 ))
+            then
                 err_msg 2 "found 99 backup files for $fn, aborting."
                 return 2
-            }
+            fi
         done
 
         # perform copy
@@ -176,13 +199,11 @@ cp-bak() (
         # check metadata consistency of copy
         _chk_stats "$fn" "$bk_fn"
 
-        if [[ $vrb -gt 1 ]]
+        if (( _verb > 1 ))
         then
             printf '\n%s\n' "Result:"
-            ls -l "$fn" "$bk_fn"
+            command ls -l "$fn" "$bk_fn"
             printf '\n'
-        else
-            true
         fi
     done
 )
